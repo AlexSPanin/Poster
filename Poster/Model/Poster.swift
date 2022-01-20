e6e6656fdd553bf45757253e03be378fa7e316ed
Panin <panin1970@gmail.com>
2022-01-20 16:52:33 +0300

    add files Poster(Model) and fetch func

---
+++ Poster/Model/Poster.swift
@@ -0,0 +1,38 @@
+//
+//  Poster.swift
+//  Poster
+//
+//  Created by Александр Панин on 20.01.2022.
+//
+
+import Foundation
+
+struct Posters: Decodable {
+    let data: Attributes?
+}
+
+struct Attributes: Decodable {
+    let attributes: [Attribute]
+    
+}
+
+struct Attribute: Decodable {
+    let slug: String?
+    let synopsis: String?
+    let userCount: Int?
+    let favoritesCount: Int?
+    let posterImage: PosterImage?
+    
+    var title: String {
+        """
+Title: \(slug ?? "")
+User Count: \(userCount ?? 0)
+Favorite User: \(favoritesCount ?? 0)
+"""
+    }
+}
+
+struct PosterImage: Decodable {
+    let tiny: String?
+    let original: String?
+}
--- Poster/Services/NetworkingManager.swift
+++ Poster/Services/NetworkingManager.swift
@@ -11,3 +11,55 @@ enum Link: String, CaseIterable {
     case kitsuApi = "https://kitsu.io/api/edge/anime?filter[text]=tokyo"
     case rickAndMortyApi = "https://rickandmortyapi.com/api/character"
 }
+
+enum ErrorNetwork: Error {
+    case errorURL
+    case errorData
+    case errorDecoding
+}
+
+class NetworkingManager {
+    static var shared = NetworkingManager()
+    private init() {}
+    
+    func fetchImage(url: String?, complition: @escaping(Result<Data, ErrorNetwork>) -> Void) {
+        
+        guard let url = URL(string: url ?? "") else {
+            complition(.failure(.errorURL))
+            return
+        }
+        
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else {
+                complition(.failure(.errorData))
+                return
+            }
+            DispatchQueue.main.async {
+                complition(.success(imageData))
+            }
+        }
+    }
+    
+    func fetchData(url: String?, complition: @escaping(Result<Posters, ErrorNetwork>) -> Void) {
+        
+        guard let url = URL(string: url ?? "") else {
+            complition(.failure(.errorURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                complition(.failure(.errorData))
+                return
+            }
+            do {
+                let model = try JSONDecoder().decode(Posters.self, from: data)
+                DispatchQueue.main.async {
+                    complition(.success(model))
+                }
+            } catch {
+                complition(.failure(.errorDecoding))
+            }
+        }.resume()
+    }
+}
