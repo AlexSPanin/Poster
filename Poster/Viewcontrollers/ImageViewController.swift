43d90979cca3abcb8f40e57fb086af8538d126d8
Panin <panin1970@gmail.com>
2022-01-21 10:18:08 +0300

    add view controllers

---
+++ Poster/Viewcontrollers/ImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageViewController.swift
+//  Poster
+//
+//  Created by Александр Панин on 21.01.2022.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
